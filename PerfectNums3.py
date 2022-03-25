fd95aaacecb952e7d20b2f87451ade8f948db6f0
Shrey Suri <shreysuri5@gmail.com>
2022-03-24 22:32:04 -0500

    Further optimization

--- PerfectNums3.py
+++ PerfectNums3.py
@@ -1,13 +1,25 @@
+import math
+
+
+def prime(a):
+   b = int(math.sqrt(a)) + 1
+   c = 2
+   while (a/c) != int(a/c) and c < b:
+       c = c + 1
+   if c == b:
+       return True
+   else:
+       return False
+   
+ 
+
 x = int(input(print("How many perfect numbers do you want printed? ")))
 y = 0
 z = 1
-d = 2
+
 
 while y < x:
     z = 2*z + 1
-    while (z/d) != int(z/d) and d < z:
-        d = d + 1
-    if d == z:
+    if prime(z) == True:
         print(int(z * (z + 1)/2))
         y = y + 1
-    d = 2
